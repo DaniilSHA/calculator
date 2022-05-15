582fc68e74394338f4a915d7c5a9e3ef86363bf0
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-05-15 13:23:19 +0300

    client

--- client/src/main/resources/static/webclient.js
+++ client/src/main/resources/static/webclient.js
@@ -1,7 +1,7 @@
-// const streamErr = e => {
-//     console.warn("error");
-//     console.warn(e);
-// }
+const streamErr = e => {
+    console.warn("error");
+    console.warn(e);
+}
 //
 // fetch("http://localhost:8082/data/5").then((response) => {
 //     return can.ndjsonStream(response.body);
@@ -17,12 +17,14 @@
 //     reader.read().then(read, streamErr);
 // });
 //
-// const render = value => {
-//     const div = document.createElement('div');
-//     div.append('stringValue:', JSON.stringify(value));
-//     document.getElementById('dataBlock').append(div);
-// };
 
+const render = value => {
+    const div = document.createElement('div');
+    div.append('stringValue:', JSON.stringify(value));
+    document.querySelector('.report-sticker').append(div);
+};
+
+let lox
 let functionFirstValue
 let functionSecondValue
 let iterationsValue
@@ -43,32 +45,22 @@ document.querySelector(".get-report-button").addEventListener("click", () => {
         body: JSON.stringify({
             firstFunction: functionFirstValue,
             secondFunction: functionSecondValue,
-            itr : iterationsValue,
-            order : orderValue
+            itr: iterationsValue,
+            order: orderValue
         })
     }).then((response) => {
-            const reader = response.body.getReader();
-            return new ReadableStream({
-                start(controller) {
-                    return pump();
-                    function pump() {
-                        return reader.read().then(({ done, value }) => {
-                            // When no more data needs to be consumed, close the stream
-                            if (done) {
-                                controller.close();
-                                return;
-                            }
-                            // Enqueue the next data chunk into our target stream
-                            controller.enqueue(value);
-                            return pump();
-                        });
-                    }
-                }
-            })
-        })
-        .then(stream => new Response(stream))
-        .then(response => console.log(response))
-
+        return can.ndjsonStream(response.body);
+    }).then(dataStream => {
+        const reader = dataStream.getReader();
+        const read = result => {
+            if (result.done) {
+                return;
+            }
+            render(result.value);
+            reader.read().then(read, streamErr);
+        }
+        reader.read().then(read, streamErr);
+    });
 
 })
 
